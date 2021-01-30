2eb308b9cb79a303943d70ec46b4808246a12f33
Nido89 <syednaveedjalal@gmail.com>
2021-01-30 07:51:24 +0200

    Random Object with StartQuiz method and int variables

--- MathGame/Form1.cs
+++ MathGame/Form1.cs
@@ -12,9 +12,40 @@ namespace MathGame
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        // creating the random number object
+        Random randomizer = new Random();
+        // Two variables to store random addition number values
+        int addNum1;
+        int addNum2;
+        // Lets create the mthod to pass values to int variables
+        public void startQuiz()
+        {
+            addNum1 = randomizer.Next(100);
+            addNum2 = randomizer.Next(100);
+
+            plusLeftLabel.Text = addNum1.ToString();
+            plusRightLabel.Text = addNum1.ToString();
+
+            sum.Value = 0;
+        }
+
+    
+
+
+    public Form1()
         {
             InitializeComponent();
+        }
+
+        private void sum_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startQuiz();
+            startButton.Enabled = false;
         }
     }
 }
